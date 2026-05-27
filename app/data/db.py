904c5716f3bc87c43ff6ff5aca47ecc9ccf7d5b3
PaoloGiuffridaa <gffpla@gmail.com>
2026-05-27 10:45:43 +0200

    added N-N relationship

--- app/data/db.py
+++ app/data/db.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import Depends
 from app.schemas.book import BookDB #noqa
 from app.schemas.users import UserDB #noqa
+from app.schemas.book_user_link import BookUserLink #noqa
 from faker import Faker #faker è una libreria che permette di generare dati casuali in varie lingue
 import os
 
@@ -34,8 +35,15 @@ def init_database():
                     birth_date=f.date_of_birth(), #genera una data di nascita casuale
                     city=f.city() #genera una città casuale
                 )
-                session.add(user) #aggiunge l'utente alla sessione del database
-            session.commit() #salva le modifiche al database    
+                session.add(user) #aggiunge l'utente alla sessione del database 
+            session.commit() #salva tutte le modifiche al database 
+            for i in range(5): #genera 5 link casuali tra libri e utenti
+                link = BookUserLink(
+                    book_id=f.pyint(min_value=1, max_value=10), #genera un id libro casuale tra 1 e 10 (assumendo che ci siano 10 libri)
+                    user_id=f.pyint(min_value=1, max_value=10) #genera un id utente casuale tra 1 e 10 (assumendo che ci siano 10 utenti)
+                )
+                session.add(link) #aggiunge il link alla sessione del database 
+            session.commit() #salva tutte le modifiche al database 
 
 
 def get_session():
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.data.db import SessionDep
+from app.schemas.book_user_link import BookUserLink
 from app.schemas.users import UserDB, UserPublic
 from app.schemas.book import BookDB, BookPublic
 from sqlmodel import select
@@ -15,6 +16,11 @@ def get_all_users(session: SessionDep) -> list[UserPublic]:
 @users_router.get("/{user_id}/books")
 def get_user_by_id(user_id: int, session: SessionDep) -> list[BookPublic]:
     """return all books of a user"""
-    statement = select(BookDB).join(UserDB).where(UserDB.id == user_id)
+    user = session.get(UserDB, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    statement = select(BookDB).join(BookUserLink).where(BookUserLink.user_id == user_id) #join tra le tabelle BookDB e BookUserLink, filtrando per user_id
     books = session.exec(statement).all()
     return books
+
+
